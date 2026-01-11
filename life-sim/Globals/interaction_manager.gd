1620ac184ab7b6edad39e4a13e5692f3465f4b21
LatonFeed <sifagamedev@gmail.com>
2026-01-11 12:59:14 +0100

    menu when clicked

--- life-sim/Globals/interaction_manager.gd
+++ life-sim/Globals/interaction_manager.gd
@@ -1,12 +1,20 @@
 extends Node
 
+# 1. LOAD THE MENU SCENE
+# This tells the manager where to find the UI scene you just built.
+const MENU_SCENE = preload("res://Scenes/action_menu.tscn")
+
 var clicked_objects: Array = []
+var active_menu = null
+
+func _ready():
+	# 2. CREATE THE MENU INSTANCE
+	# We create the menu immediately but keep it hidden (the menu script hides itself in _ready)
+	active_menu = MENU_SCENE.instantiate()
+	add_child(active_menu)
 
 func register_click(object_that_was_clicked):
 	clicked_objects.append(object_that_was_clicked)
-	
-	# Wait one frame. This allows all overlapping objects 
-	# to register themselves
 	call_deferred("_process_clicks")
 
 func _process_clicks():
@@ -14,14 +22,16 @@ func _process_clicks():
 		return
 
 	var all_actions = []
-	
 	for obj in clicked_objects:
 		all_actions.append_array(obj.available_actions)
 	
-	# Debug
-	print("OPENING MENU WITH ACTIONS: ", all_actions)
+	# 3. CALCULATE POSITION
+	# Get the mouse coordinates on the screen
+	var mouse_pos = get_viewport().get_mouse_position()
 	
-	# TODO: UI Scene
-	# show_action_menu(all_actions)
-
+	# 4. SHOW THE MENU
+	if active_menu:
+		# We pass the position and the list of actions to the UI
+		active_menu.show_menu(mouse_pos, all_actions)
+	
 	clicked_objects.clear()
---
+++ life-sim/Scenes/action_menu.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+@onready var menu_background = $MenuBackground
+@onready var button_list = $MenuBackground/ButtonList
+
+func _ready():
+	menu_background.visible = false
+
+func show_menu(screen_position: Vector2, actions: Array):
+	# 1 Move the menu to the mouse position
+	menu_background.global_position = screen_position
+	
+	# 2 Clear previous
+	for child in button_list.get_children():
+		child.queue_free()
+		
+	# 3 Create a new button for each action
+	for action_name in actions:
+		var btn = Button.new()
+		btn.text = action_name
+		
+		btn.pressed.connect(_on_button_clicked.bind(action_name))
+		
+		button_list.add_child(btn)
+	
+	# 4 Show the menu
+	menu_background.visible = true
+
+func _on_button_clicked(action_name):
+	print("Selected action: ", action_name)
+	menu_background.visible = false
